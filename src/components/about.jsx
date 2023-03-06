e694baf8d7e0a2701b2eb9061e659eabb4bf07cd
musense <musense.ad@gmail.com>
2023-03-06 15:32:14 +0800

    about page refractoring, add package aos

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,25 +1,65 @@
-import React from 'react';
-import styles from './../css/about.module.css';
+import React, { useEffect, useState } from 'react';
+// import styles from './../css/about.module.css';
+import './../css/about.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export default function About() {
+  useEffect(() => {
+    AOS.init({
+      offset: 120,
+      delay: 50, // values from 0 to 3000, with step 50ms
+      duration: 400, // values from 0 to 3000, with step 50ms);
+      easing: 'ease-in-out',
+    });
+  }, []);
   return (
-    <div className={styles.bg}>
-      <div className={styles.slogan} />
-      <div id="about" className={styles['top-image']} />
+    <div className='about'>
+      <div className='bg' />
+      <div className='about-box'>
+        <div className='slogan' />
+        <div
+          id='about'
+          className='top-image'
+        />
+      </div>
 
-      <div className={styles.welcome} />
-      <div className={styles.free} />
+      <div className='welcome' />
+      {/* <div className='free' /> */}
 
-      <div className={styles['orange-bubble']} />
-      <div className={styles['orange-bubble_2']} />
-      <div className={styles['blue-bubble']} />
-      <div className={styles['blue-bubble_2']} />
+      <div className='bubble-box'>
+        <div>
+          <div
+            className='orange-bubble-left'
+            data-aos='orange-slide-left'
+            data-aos-delay='0'
+          />
+          <div
+            className='orange-bubble-right'
+            data-aos='orange-slide-right'
+            data-aos-delay='400'
+          />
+        </div>
+        <div>
+          <div
+            className='blue-bubble-left'
+            data-aos='blue-slide-left'
+            data-aos-delay='600'
+          />
+          <div
+            className='blue-bubble-right'
+            data-aos='blue-slide-right'
+            data-aos-delay='1000'
+          />
+        </div>
+      </div>
 
-      <div className={styles['musense-can-help']} />
-      <div className={styles.reply} />
-
-      <div className={styles['triangle-range-orange']} />
-      <div className={styles['triangle-range-orange_2']} />
+      <div className='musense-can-help' />
+      <div className='reply' />
+      <div className='triangle-box'>
+        <div className='triangle-range-orange-right' />
+        <div className='triangle-range-orange-left' />
+      </div>
     </div>
   );
 }
