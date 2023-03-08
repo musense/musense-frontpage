df69a14a2f7e5b3d81fd8ae1030526ceafdfceda
musense <musense.ad@gmail.com>
2023-03-08 11:35:55 +0800

    add packages for postcss

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import postcssImport from "postcss-import"
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  css:{
+    postcss:{
+      plugins:[
+        postcssImport,
+        autoprefixer,
+      ]
+    }
+  },
+  plugins: [
+    react(),
+  ],
+  base: "./",
+  esbuild: {
+    pure: ['console.log'],
+    minify: true
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+        // manualChunks(id) {
+        //   if (id.includes('node_modules')) {
+        //     return id
+        //       .toString()
+        //       .split('node_modules/')[1]
+        //       .split('/')[0]
+        //       .toString()
+        //   }
+        // }
+      }
+    }
+  }
 })
